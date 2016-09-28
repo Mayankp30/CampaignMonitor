b427d7664cd4cf10935dcf73858c3f8188aee2de
Mayank Purohit <mayankpurohit@outlook.com>
2016-09-28 00:27:03 -0400

    fetch words from file -updated

--- src/DerivedTest2.java
+++ src/DerivedTest2.java
@@ -1,33 +1,68 @@
 /**
  * Created by Mayank on 9/25/16.
  */
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashSet;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class DerivedTest2 {
 
-    public static int foo(int[] array, int k) {
-        HashSet<Integer> myset = new HashSet<>();
-        Arrays.sort(array);
-        for(int i : array){
-            myset.add(i);
+    public void countMaxOccurrence(String[] words){
+        HashMap<String,Integer> map = new HashMap<>();
+        for (String s:words) {
+            if(!map.containsKey(s)){
+                map.put(s,1);
+            }else{
+                map.put(s,map.get(s)+1);
+            }
+        }
+        int high = Integer.MIN_VALUE;
+        for (int mvalue:map.values()){
+            if(mvalue>high){
+                high=mvalue;
+            }
+        }
+        System.out.println(high);
+        for(Map.Entry<String,Integer> entry: map.entrySet()){
+            if(entry.getValue()==high){
+                System.out.println(entry.getKey());
+            }
         }
 
 
-        int[] result = new int[myset.size()];
-        int value=0;
-        for(int j : myset){
-            result[value++]=j;
-        }
 
-        return result[result.length-k];
 
     }
 
 
+
+
     public static void main(String args[]) {
-       int[] array = {3,3,4,4,5,8,9,10,11,2,4};
-        System.out.println(foo(array,4));
+        DerivedTest2 demo1 = new DerivedTest2();
+
+        try( Scanner sc = new Scanner(new FileReader("/Users/Mayank/IdeaProjects/Gnop/src/random.txt"))) {
+
+            ArrayList<String> list = new ArrayList<>();
+            while (sc.hasNext()) {
+                list.add(sc.next());
+            }
+
+           String[] result = new String[list.size()];
+            int value=0;
+            for (String s: list) {
+                result[value++]=s;
+            }
+
+            demo1.countMaxOccurrence(result);
+            //System.out.println(result.length);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
